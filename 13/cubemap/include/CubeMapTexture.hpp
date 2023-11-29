7ab94f3a19b9ed11c26081413b5ca7afb27c1803
MikeShah <mshah.475@gmail.com>
2023-11-29 11:32:56 -0500

    cubemap example

---
+++ 13/cubemap/include/CubeMapTexture.hpp
@@ -0,0 +1,42 @@
+/** @file CubeMapTexture.hpp
+ *  @brief Loads an image and creates an OpenGL texture on the GPU..
+ *  
+ *  More...
+ *
+ *  @author Mike
+ *  @bug No known bugs.
+ */
+#ifndef CUBE_MAP_TEXTURE_HPP
+#define CUBE_MAP_TEXTURE_HPP
+
+#include "Image.hpp"
+
+#include <glad/glad.h>
+#include <string>
+
+class CubeMapTexture{
+public:
+    // Constructor
+    CubeMapTexture();
+    // Destructor
+    ~CubeMapTexture();
+	// Loads and sets up an actual texture
+    void LoadCubeMapTexture(const std::string filepath);
+	// slot tells us which slot we want to bind to.
+    // We can have multiple slots. By default, we
+    // will set our slot to 0 if it is not specified.
+    void Bind(unsigned int slot=0) const;
+    // Be done with our texture
+    void Unbind();
+private:
+    // Store a unique ID for the texture
+    GLuint m_textureID;
+	// Filepath to the image loaded
+    std::string m_filepath;
+    // Store whatever image data inside of our texture class.
+    Image* m_image;
+};
+
+
+
+#endif
