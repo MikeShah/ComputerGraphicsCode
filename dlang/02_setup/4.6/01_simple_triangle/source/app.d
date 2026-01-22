46de85f0e349efb5bf0722073d80f79238b47e08
MikeShah <mshah.475@gmail.com>
2026-01-22 14:48:47 -0500

    triangle

---
+++ dlang/02_setup/4.6/01_simple_triangle/source/app.d
@@ -0,0 +1,11 @@
+/// @file: 01_simple_triangle/app.d
+
+import graphics_app;
+
+/// Program entry point 
+/// NOTE: When debugging, this is '_Dmain'
+void main(string[] args)
+{
+    GraphicsApp app = GraphicsApp(640,480);
+    app.Loop();
+}
--- dlang/02_setup/4.6/01_simple_triangle/source/graphics_app.d
+++ dlang/02_setup/4.6/01_simple_triangle/source/graphics_app.d
@@ -269,27 +269,35 @@ extern(C) void message_callback(GLenum source, GLenum type, GLuint id, GLenum se
 	auto type_str = (GLenum type) {
 		switch (type)
 		{
-		case GL_DEBUG_TYPE_ERROR: return "ERROR";
-		case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: return "DEPRECATED_BEHAVIOR";
-		case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: return "UNDEFINED_BEHAVIOR";
-		case GL_DEBUG_TYPE_PORTABILITY: return "PORTABILITY";
-		case GL_DEBUG_TYPE_PERFORMANCE: return "PERFORMANCE";
-		case GL_DEBUG_TYPE_MARKER: return "MARKER";
-		case GL_DEBUG_TYPE_OTHER: return "OTHER";
-      default: assert(0);
+      case GL_DEBUG_TYPE_ERROR: return "ERROR";
+      case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: return "DEPRECATED_BEHAVIOR";
+      case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: return "UNDEFINED_BEHAVIOR";
+      case GL_DEBUG_TYPE_PORTABILITY: return "PORTABILITY";
+      case GL_DEBUG_TYPE_PERFORMANCE: return "PERFORMANCE";
+      case GL_DEBUG_TYPE_MARKER: return "MARKER";
+      case GL_DEBUG_TYPE_OTHER: return "OTHER";
+      default: return "not sure";
 		}
 	};
 
 	auto severity_str = (GLenum severity) {
 		switch (severity) {
-		case GL_DEBUG_SEVERITY_NOTIFICATION: return "NOTIFICATION";
-		case GL_DEBUG_SEVERITY_LOW: return "LOW";
-		case GL_DEBUG_SEVERITY_MEDIUM: return "MEDIUM";
-		case GL_DEBUG_SEVERITY_HIGH: return "HIGH";
-      default: assert(0);
+      case GL_DEBUG_SEVERITY_NOTIFICATION: return "NOTIFICATION";
+      case GL_DEBUG_SEVERITY_LOW: return "LOW";
+      case GL_DEBUG_SEVERITY_MEDIUM: return "MEDIUM";
+      case GL_DEBUG_SEVERITY_HIGH: return "HIGH";
+      default: return "not sure";
 		}
 	};
 
+  auto result_src = src_str(source);
+  auto result_type= type_str(type);
+  auto result_severity = severity_str(severity);
+
   // Provide some basic help 
-  debug	writeln(src_str,", ",type_str,", ",severity_str,", ", id, ": ", cast(string)message[0..length]);
+  debug writeln(`=====REPORT FROM DEBUG CALLBACK========`);
+  debug writeln(`Note: Sometimes these are errors, sometimes these are just notifications from the driver`);
+  debug writeln(`Note: Notifications are often fine behavior for example, and you can filter/ignore them`);
+  debug	writeln(result_src,", ",result_type,", ",result_severity,", id=", id, "\n\tmessage: ", cast(string)message[0..length]);
+  debug writeln(`=======================================`);
 }
