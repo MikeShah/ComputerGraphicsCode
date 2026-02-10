b95757f59235de58bed054cd2c7b8194051e1348
MikeShah <mshah.475@gmail.com>
2026-02-10 17:42:51 -0500

    rename and small changes

--- dlang/10_scene_tree/object3d.d
+++ dlang/10_scene_tree/object3d.d
@@ -1,15 +1,48 @@
 // @file: object3d.d
 // NOTE: I named this 'object3d.d' because 'object.d' is a file
 //       used in the D language for classes.
+//       Other good names might be 'Entity3D', 'Actor3D', etc.
+
+// Added purely so that this code can compile.
+alias GLuint = uint;
+alias mat4   = float[16];
+alias Texture = uint;
 
 struct Object3D{
+  // Handles to named opengl objects
 	GLuint mVAO;
 	GLuint mVBO;
-	GLUint mIBO;
+	GLuint mIBO;
+  // Shader/material handle
+	GLuint mShaderID;
 
-	GLUint mShaderID;
+  // Transformations
+	mat4 mModelMatrix;
 
+  // Texture references
 	Texture[] mTextures;
 
-	mat4 mModelMatrix;
+
+
+
+  void DoSomeTransformation(){}
+}
+
+
+void main(){
+
+  Object3D myObject1;
+  Object3D myObject2;
+  Object3D myObject3;
+  Object3D myObject4;
+  Object3D myObject5;
+  Object3D myObject6;
+  Object3D myObject7;
+  Object3D myObject8;
+  Object3D myObject9;
+  Object3D myObject10;
+  // etc.
+
+
+ 
 }
--- dlang/10_scene_tree/objectmanager.d
+++ dlang/10_scene_tree/objectmanager.d
@@ -8,32 +8,48 @@ class ObjectManager{
 	static GetObject3D(size_t index){
 		return mObjects[index];
 	}
-	/// Add a new object
+
+  // 'Factory' Method to create 3D Objects
+  static Object3D Create(T...)(T params){
+    Object3D result;
+    /// ...
+    return result;
+  }
+
+	/// Add a new object to the manager
 	static void AddObject3D(Object3D o){
 		mObjects ~= o;
 	}
 	/// Remove all objects
-	static void RemoveAll(){
-			// ...
-	}
+	static void RemoveAll(){ /* ... */ }
 	/// Update all of the objects
-	static void UpdateAll(){
-		// ...
-	}
+	static void UpdateAll(){	/* ... */ }
 	/// Render all of the objects
-	static void RenderAll(){
-		// ...
-	}
+	static void RenderAll(){	/* ... */	}
 	
 	// Dynamic array to store all of our objects
 	static Object3D[] mObjects;
 }
 
-
 void MainGameLoop(){
+
+  // Create an object manually or from some configuration file
+  auto newObject = ObjectManager.Create();  
+
+  // Add the object to our manager
+  ObjectManager.AddObject3D(newObject);
+
+  // Do something interesting with our collection of objects
 	// ...
 	ObjectManager.GetObject3D(1).DoSomeTransformation();
 	ObjectManager.GetObject3D(2).DoSomeTransformation();
 	ObjectManager.GetObject3D(3).DoSomeTransformation();
 	// ...
+  
+  // Perform some 'mutable' operation on an object.
+  // That is why we have the 'ref' on 'o'
+  foreach(ref o ; ObjectManager.mObjects){
+     o.DoSomeTransformation();
+  }
+
 }
